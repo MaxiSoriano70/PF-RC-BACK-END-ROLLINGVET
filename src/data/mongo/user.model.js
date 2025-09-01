283bbd30be85d0a028798112f8caaf477b855203
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2025-09-01 04:10:11 -0300

    Agregando modelo de user

---
+++ src/data/mongo/user.model.js
@@ -0,0 +1,48 @@
+import { Schema, model } from "mongoose";
+import { USER_ROLES } from "../../emuns/userRoles.emun.js";
+
+const collection = "users";
+
+const userSchema = new Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [
+            /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+            "El email no tiene un formato válido."
+        ]
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+        validate: {
+            validator: function (v) {
+                return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$/.test(v);
+            },
+            message: props => `La contraseña no cumple con los requisitos de seguridad.`
+        }
+    },
+    role: {
+        type: String,
+        enum: Object.values(USER_ROLES),
+        default: USER_ROLES.USER
+    },
+    telefono: {
+        type: String,
+        required: true
+    },
+    fotoPerfil: {
+        type: String,
+        default: "https://i.ibb.co/2kR9YQk/default-profile.png"
+    }
+});
+
+export default model(collection, userSchema);
