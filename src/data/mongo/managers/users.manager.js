e8efba9125b5a841b07b301bf70002a21c60608b
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2025-09-07 23:38:37 -0300

    1er CRUD de user terminado

--- src/data/mongo/managers/users.manager.js
+++ src/data/mongo/managers/users.manager.js
@@ -2,7 +2,7 @@ import User from "../models/users.model.js";
 
 class UsersManager {
     create = async (data) => await User.create(data);
-    readAll = async () => await User.find(filter).lean();
+    readAll = async (filter = {}) => await User.find(filter).lean();
     readById = async (id) => await User.findById(id).lean();
     updateById = async (id, data) => await User.findByIdAndUpdate(id, data, { new: true });
     destroyById = async (id) => await User.findByIdAndDelete(id);
--- src/routes/api/user.router.js
+++ src/routes/api/user.router.js
@@ -16,30 +16,79 @@ const createOne = async (req, res, next) => {
         next(error);
     }
 };
-const getAll = (req, res, next) => {
+
+const getAll = async (req, res, next) => {
     try {
-        
+        const filter = req.query;
+        const response = await usersManager.readAll(filter);
+        if (response.length === 0) {
+            const error = new Error("No users found");
+            error.status = 404;
+            throw error;
+        };
+        res.status(200).json({
+            data: response,
+            method: req.method,
+            url: req.url
+        });
     } catch (error) {
         next(error);
     }
 };
-const getOneById = (req, res, next) => {
+
+const getOneById = async (req, res, next) => {
     try {
-        
+        const { uid } = req.params;
+        const response = await usersManager.readById(uid);
+        if (!response) {
+            const error = new Error("User not found");
+            error.status = 404;
+            throw error;
+        };
+        res.status(200).json({
+            data: response,
+            method: req.method,
+            url: req.url
+        });
     } catch (error) {
         next(error);
     }
 };
-const updateOneById = (req, res, next) => {
+
+const updateOneById = async (req, res, next) => {
     try {
-        
+        const { uid } = req.params;
+        const data = req.body;
+        const response = await usersManager.updateById(uid, data);
+        if (!response) {
+            const error = new Error("User not found");
+            error.status = 404;
+            throw error;
+        };
+        res.status(200).json({
+            data: response,
+            method: req.method,
+            url: req.url
+        });
     } catch (error) {
         next(error);
     }
 };
-const deleteOneById = (req, res, next) => {
+
+const deleteOneById = async (req, res, next) => {
     try {
-        
+        const { uid } = req.params;
+        const response = await usersManager.destroyById(uid);
+        if (!response) {
+            const error = new Error("User not found");
+            error.status = 404;
+            throw error;
+        };
+        res.status(200).json({
+            data: response,
+            method: req.method,
+            url: req.url
+        });
     } catch (error) {
         next(error);
     }
@@ -47,8 +96,8 @@ const deleteOneById = (req, res, next) => {
 
 routerUser.post("/", createOne);
 routerUser.get("/", getAll);
-routerUser.get("/:id", getOneById);
-routerUser.put("/:id", updateOneById);
-routerUser.delete("/:id", deleteOneById);
+routerUser.get("/:uid", getOneById);
+routerUser.put("/:uid", updateOneById);
+routerUser.delete("/:uid", deleteOneById);
 
 export default routerUser;
